ad68a042ad65062fe1cdb20afda9f14373f03be9
Jacqueline Remmel <jpremmel@gmail.com>
2019-08-30 09:09:59 -0700

    Add js method to calculate pizza price.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,20 @@ function Pizza(size, toppings) {
   this.size = size,
   this.toppings = toppings
 }
+
+Pizza.prototype.calcPrice = function() {
+  var price = 0;
+  if (this.size === "Personal") {
+    price += 5;
+  } else if (this.size === "Small") {
+    price += 8;
+  } else if (this.size === "Medium") {
+    price += 12;
+  } else if (this.size === "Large") {
+    price += 16;
+  } else if (this.size === "Extra Large") {
+    price += 20;
+  }
+  price += this.toppings.length;
+  return price;
+}
